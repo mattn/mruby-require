12a3f01ebf19c1c3aab9fbd0e481fe8b4457d652
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-04-30 23:20:56 +0900

    fix typo

--- run_test.rb
+++ run_test.rb
@@ -3,7 +3,7 @@
 if __FILE__ == $PROGRAM_NAME
   require 'fileutils'
   FileUtils.mkdir_p 'tmp'
-  unless File.exists?('tmp/mruby')
+  unless File.exist?('tmp/mruby')
     system 'git clone https://github.com/mruby/mruby.git tmp/mruby'
   end
   exit system(%Q[cd tmp/mruby; MRUBY_CONFIG=#{File.expand_path __FILE__} ./minirake #{ARGV.join(' ')}])
